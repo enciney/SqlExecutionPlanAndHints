5b69ff10655a9ae8dccaac5f1b7005c1dcee4506
EnginYıldırım <enginyldrm02@gmail.com>
2019-12-24 18:32:10 +0300

    nhibernate config added

--- SqlExecutionPlanAndHints/SqlExecutionPlanAndHints/Program.cs
+++ SqlExecutionPlanAndHints/SqlExecutionPlanAndHints/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NHibernate;
+using System;
+using System.Configuration;
+using System.IO;
 
 namespace SqlExecutionPlanAndHints
 {
+	
 	class Program
 	{
+		static IConfigurationRoot appConfig { get; set; }
+		static ISessionFactory sessionFactory { get; set; }
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			StartUpActions();
+		}
+
+
+		static void StartUpActions()
+		{
+
+			// .netCore itself Dependency Injection
+			var serviceProvider = new ServiceCollection()
+				.AddSingleton<IConfigurationBuilder>(new ConfigurationBuilder()
+			   .SetBasePath(Directory.GetCurrentDirectory())
+			   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+			   );
+			var serviceBuilder = serviceProvider.BuildServiceProvider();
+			appConfig = serviceBuilder.GetService<IConfigurationBuilder>().Build();
+			var nhConfigPath = appConfig.GetValue<string>("nhConfig");
+			#region autofac DI
+			// classic Autofac
+			//var builder = new ContainerBuilder();
+			//builder.RegisterInstance(new ConfigurationBuilder()
+			//   .SetBasePath(Directory.GetCurrentDirectory())
+			//   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)).As<IConfigurationBuilder>();
+			//var container = builder.Build();
+			//using (var scope = container.BeginLifetimeScope())
+			//{
+			//	appConfig = scope.Resolve<IConfigurationBuilder>().Build();
+			//}
+
+			//nhConfig = appConfig.GetValue<string>("nhConfig");
+			#endregion
+			// try to configure nHibernate
+			var configurationObj = new NHibernate.Cfg.Configuration();
+			var nhConfiguration = nhConfigPath == null ? configurationObj.Configure() : configurationObj.Configure(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, nhConfigPath));
+
+			sessionFactory = nhConfiguration.BuildSessionFactory();
+			var session = sessionFactory.OpenSession();
+			var count = session.CreateSQLQuery("select count(*) from all_cells").List();
+			session.Close();
 		}
 	}
 }
